cf286d7d38618ff94b106938ed68591797078387
zxins <624042309@qq.com>
2020-04-05 10:51:17 +0800

    gitignore

--- short/config.py
+++ short/config.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:zx3620382@localhost:3306/short_url"
+DB_HOST = 'localhost'
+DB_USER = ''
+DB_PASS = ''
+DB_PORT = 3306
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{0}:{1}@{2}:{3}/short_url".format(DB_USER, DB_PASS, DB_HOST, DB_PORT)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
